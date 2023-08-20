d32c21e145d566af99c897cacad6812b0f4772c5
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2023-08-19 20:27:53 -0400

    Update btcpay_server_instance.py
    converted to to work with nodeless.io

--- models/btcpay_server_instance.py
+++ models/btcpay_server_instance.py
@@ -23,44 +23,23 @@ class BTCPayServerInstance(models.Model):
     conversion_rate_source = fields.Char(string='Conversion Rate', readonly=True) #should be conversion rate source
     expiration_minutes = fields.Integer('Expiration Minutes') #seconds for lightning invoice, converted in function
 
-
-    def action_get_conversion_rate_source(self): #gets conversion rate source as set on BTCpay server
+    def action_get_stores(self):  # gets store IDs on nodeless server
         try:
-            server_url = self.server_url + "/api/v1/stores/" + self.store_id + "/rates/configuration"
-            headers = {"Authorization": "Token %s" % (self.api_key)}
+            server_url = self.server_url + "/api/v1/store"
+            headers = {"Authorization": "Bearer %s" % (self.api_key), "Content-Type": "application/json",
+                       "Accept": "application/json"}
             response = requests.request(method="GET", url=server_url, headers=headers)
             response_json = response.json()
-            result = response_json['preferredSource'] if response.status_code == 200 else None
+            result = response_json['data'] if response.status_code == 200 else None
             return result
         except Exception as e:
             raise UserError(_("Get Conversion Rate Source: %s", e.args))
 
-    def action_get_conversion_rate(self): #obtains conversion rate from BTCpay server
-        try:
-            server_url = self.server_url + "/api/v1/stores/" + self.store_id + "/rates"
-            headers = {"Authorization": "Token %s" % (self.api_key)}
-            response = requests.request(method="GET", url=server_url, headers=headers)
-            response_json = response.json()
-            result = response_json[0]['rate'] if response.status_code == 200 else None
-            return result
-        except Exception as e:
-            raise UserError(_("Get Conversion Rate: %s", e.args))
 
-    def get_amount_sats(self, pos_payment_obj): #obtains amount of satoshis to invoice by calling action_get_conversion_rate and and doing the math, returns dict of both values
+    def test_nodeless_server_connection(self): #test connection to nodelessserver eg are server_url, and api_key correct
         try:
-            btcpay_conversion_rate = self.action_get_conversion_rate()
-            amount_sats = round((float(pos_payment_obj.get('amount')) / float(btcpay_conversion_rate)) * 100000000, 1) #conversion to satoshis and rounding to one decimal
-            invoiced_info = {'conversion_rate': btcpay_conversion_rate,
-                             'invoiced_sat_amount': amount_sats
-                             }
-            return invoiced_info #return dictionary with results of both functions
-        except Exception as e:
-            raise UserError(_("Get Millisat amount: %s", e.args))
-
-    def test_btcpay_server_connection(self): #test connection to btcpayserver eg are server_url, and api_key correct
-        try:
-            server_url = self.server_url + "/api/v1/api-keys/current"
-            headers = {"Authorization": "Token %s" % (self.api_key)}
+            server_url = self.server_url + "/api/v1/status"
+            headers = {"Authorization": "Bearer %s" % (self.api_key), "Content-Type": "application/json", "Accept": "application/json"}
             response = requests.request(method="GET", url=server_url, headers=headers)
             is_success = True if response.status_code == 200 else False
             return is_success #returns boolean
@@ -68,7 +47,7 @@ class BTCPayServerInstance(models.Model):
             raise UserError(_("Test Connection Error: %s", e.args))
 
     def action_test_connection(self): # turns test_btcpay_server_connection into a message for user
-        is_success = self.test_btcpay_server_connection()
+        is_success = self.test_nodeless_server_connection()
         type = (
             "success"
             if is_success
@@ -93,9 +72,9 @@ class BTCPayServerInstance(models.Model):
         }
 
     def action_activate(self): #activates the btcpay server instance to be the currently used credentials and
-        is_success = self.test_btcpay_server_connection() #tests connection
+        is_success = self.test_nodeless_server_connection() #tests connection
         if is_success:
-            self.conversion_rate_source = self.action_get_conversion_rate_source() #gets conversion rate source for reference
+            self.conversion_rate_source = self.action_get_stores() #gets conversion rate source for reference
             self.state = 'active'
             # Auto create Account Journal and POS Payment Method at the first Activate
             journal = self.env['account.journal'].search(
@@ -120,34 +99,36 @@ class BTCPayServerInstance(models.Model):
     def action_deactivate(self):
         self.state = 'inactive'
 
-    def action_create_invoice_lightning(self, pos_payment_obj): #creates lightning invoice
+    def action_create_invoice_lightning(self, pos_payment_obj):  # creates lightning invoice
         try:
-            invoiced_info = self.get_amount_sats(pos_payment_obj) # gets the invoiced satoshi amount and conversion rate from get_amount_sats function
-            amount_millisats = invoiced_info['invoiced_sat_amount'] * 1000 #converts sats to millisats as required by btcpayserver
-            server_url = self.server_url + "/api/v1/stores/" + self.store_id + "/lightning/BTC/invoices"
-            headers = {"Authorization": "Token %s" % (self.api_key), "Content-Type": "application/json"}
-            lightning_expiration_minutes = self.expiration_minutes * 60 #conversion of expiration time from min to sec for submission to btcpay server
+            server_url = self.server_url + "/api/v1/store/" + self.store_id + "/invoice"
+            headers = {"Authorization": "Bearer %s" % (self.api_key), "Content-Type": "application/json",
+                       "Accept": "application/json"}
             payload = {
-                "amount": amount_millisats,
-                "description": f"{self.btcpay_company_name} {pos_payment_obj.get('order_name')}", #desciption for customer - company name and order name
-                "expiry": lightning_expiration_minutes,
+                "amount": pos_payment_obj.get('amount'),
+                "currency": "USD",
             }
             response = requests.post(server_url, data=json.dumps(payload), headers=headers)
             response_json = response.json()
-            result = response_json if response.status_code == 200 else None
-            result.update(invoiced_info) #attach invoiced info (sat amount and conversion rate to API response
-            return result #returns merged resuls
+            result = response_json if response.status_code == 201 else None
+            conversion_rate = round(pos_payment_obj.get('amount') / (result['data'].get('satsAmount') / 100000000), 2)
+            result.update({'invoiced_sat_amount': result['data'].get('satsAmount'),
+                           'conversion_rate': conversion_rate})  # attach invoiced info (sat amount and conversion rate to API response
+            return result  # returns merged resuls
         except Exception as e:
-            raise UserError(_("Create BTCPay Lightning Invoice: %s", e.args))
+            _logger.info('triggered action create invoice lightning - exception')
+            raise UserError(_("Create nodeless Lightning Invoice: %s", e.args))
 
-
-    def action_check_lightning_invoice(self, lightning_invoice_id): #checks status of lightning invoices, only
+    def action_check_lightning_invoice(self, lightning_invoice_id):  # checks status of lightning invoices, only
         try:
-            server_url = self.server_url + "/api/v1/stores/" + self.store_id + "/lightning/BTC/invoices/" + lightning_invoice_id
-            headers = {"Authorization": "Token %s" % (self.api_key), "Content-Type": "application/json"}
+            server_url = self.server_url + "/api/v1/store/" + self.store_id + "/invoice/" + lightning_invoice_id + "/status"
+            headers = {"Authorization": "Bearer %s" % (self.api_key), "Content-Type": "application/json",
+                       "Accept": "application/json"}
             response = requests.request(method="GET", url=server_url, headers=headers)
             response_json = response.json()
             result = response_json if response.status_code == 200 else None
+            _logger.info(response.status_code)
+            _logger.info(response_json)
             return result
         except Exception as e:
-            raise UserError(_("Check BTCPay Lightning Invoice: %s", lightning_invoice_id, e.args))
+            raise UserError(_("Check nodeless Lightning Invoice: %s", lightning_invoice_id, e.args))
